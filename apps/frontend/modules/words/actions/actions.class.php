56d31337a6db1eb55a71f5265820a46920f24e1e
Jonathan Wohl <jon@jonwohl.com>
2010-05-06 00:47:10 -0500

    Added module files to working copy.

---
+++ apps/frontend/modules/words/actions/actions.class.php
@@ -0,0 +1,27 @@
+<?php
+
+/**
+ * words actions.
+ *
+ * @package    greight
+ * @subpackage words
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class wordsActions extends sfActions
+{
+ /**
+  * Executes index action
+  *
+  * @param sfRequest $request A request object
+  */
+  public function executeIndex(sfWebRequest $request)
+  {
+    $this->forward('default', 'module');
+  }
+  
+  public function executeView(sfWebRequest $request)
+  {
+
+  }
+}
